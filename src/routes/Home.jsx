fe7c991c8c4224c74e3628c453cbcd53ca04a437
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2023-11-08 17:03:55 -0300

    carregamento de dados e estilização da página home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
                 "https://jsonplaceholder.typicode.com/posts")
 
             const data = response.data
-            console.log(data)
+            setPosts(data)
 
         } catch (error) {
             console.log(error)
@@ -30,7 +30,21 @@ const Home = () => {
 
     }, [])
 
-    return <div>Home</div>
+    return (
+    <div className="home">
+        <h1>Últimos posts</h1>
+        {posts.length === 0 ? <p>Carregando...</p> : (
+            posts.map((post) => (
+                <div className="post" key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                    <Link to={`/posts/${post.id}`} className="btn">
+                        Ler mais
+                    </Link>
+                </div>
+            ))
+        )}
+    </div>)
 }
 
 export default Home
